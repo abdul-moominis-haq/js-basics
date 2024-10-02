1e81c9feda51ac29f1a38cf95211bf515edf4767
abdulmoominishaq21@gmail.com <114371157+abdul-moominis-haq@users.noreply.github.com>
2024-10-02 03:43:00 +0000

    Logs out max and min of values to the console

--- logAtLeastFive(5).js
+++ logAtLeastFive(5).js
@@ -1,7 +1,8 @@
 function logAtLeastfive(n){
     console.log("Log At Least Five numbers")
+
     for(let i =1; i <= Math.max(5,n); i++){
         console.log(i);
     }
 }
-logAtLeastfive(6)
+logAtLeastfive(10)
---
+++ logAtMostFive.js
@@ -0,0 +1,8 @@
+function logAtMoststfive(n){
+    console.log("Log At Mostt Five numbers")
+
+    for(let i =1; i <= Math.min(5,n); i++){
+        console.log(i);
+    }
+}
+logAtMoststfive(4)
